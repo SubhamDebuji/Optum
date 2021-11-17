a423e586fc08c4bab3e5550a0f568afa580d83f3
SubhamGupta007 <subhamyoyo23@gmail.com>
2021-11-17 14:46:59 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import { Route } from "react-router-dom"
 function App() {
   return (
     <>
+      <Header />
+      <Footer />
     </>
   );
 }
